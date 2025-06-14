c2447a5b969d60c0a1c2477551de59301493f453
Abraham Olufeyitimi <abrahamdammy1@gmail.com>
2025-06-13 22:05:05 -0300

    add the styling for the welcome page

--- client/src/components/WelcomePage/WelcomePage.jsx
+++ client/src/components/WelcomePage/WelcomePage.jsx
@@ -1,27 +1,32 @@
 import "./WelcomePage.scss"
 import Button from "../Button/Button";
+import Girl from "../../assets/images/Toy-Girl.svg";
+import Boy from "../../assets/images/Toy-Boy.svg";
+import Hat from "../../assets/logo/Chef-Logo.svg";
+
 
 const WelcomePage = () => {
     return (
         <>
             <div className="welcome">
                 <div className="welcome__img">
-                    <img className="welcome__logo"></img>
+                    <img className="welcome__logo" src={Hat}/>
                 </div>
 
                 <div className="welcome__msg">
                     <h1 className="welcome__text">Food for Everyone</h1>
                 </div>
 
-                <div className="welcome__item">
-                    <img className="welcome__lady"></img>
+                <div className="welcome__pics">
+                    <img className="welcome__lady" src={Girl}/>
+                    <img className="welcome__man" src={Boy}/>
+                    <div className="welcome__block">
+
+                    </div>
                 </div>
 
-                <div className="welcome__pic">
-                    <img className="welcome__man"></img>
-                </div>
 
-                <Button>Get started</Button>
+                <Button className="btn" variant="secondary">Get started</Button>
             </div>
         </>
     )
